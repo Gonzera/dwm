825b6adb54d5a8a921a4251548c829b723b7e37c
Gonzera <egongamertermal@gmail.com>
2021-05-09 17:14:54 -0300

    actualfullscreen patch

--- config.h
+++ config.h
@@ -2,18 +2,18 @@
 
 /* appearance */
 static const unsigned int borderpx  = 4;        /* border pixel of windows */
-static const unsigned int gappx     = 10;        /* gaps between windows */
+static const unsigned int gappx     = 25;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "RobotoMono:size=10" };
+static const char dmenufont[]       = "RobotoMono:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-#include "/home/gonzera/.cache/wal/colors-wal-dwm.h"
+#include "/home/gon/.cache/wal/colors-wal-dwm.h"
 
 //static const char *colors[][3]      = {
 	/*               fg         bg         border   */
@@ -22,7 +22,7 @@ static const char col_cyan[]        = "#005577";
 //};
 
 /* tagging */
-static const char *tags[] = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+static const char *tags[] = { "一", "二", "三", "四", "五", "六", "七", "八", "九" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -60,7 +60,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *volup[]  = { "amixer", "set", "Master", "5%+"};
 static const char *voldown[]  = { "amixer", "set", "Master", "5%-"};
 static const char *volmute[]  = { "amixer", "set", "Master", "toggle"};
@@ -89,6 +89,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY|ShiftMask,		XK_f,	   togglefullscr,  {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
@@ -103,7 +104,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = playernext } },
 	{ MODKEY|ShiftMask,             XK_j,      spawn,          {.v = playerprevious } },
 	{ 0,               	        XK_Print,  spawn,          {.v = printsc } },
-	{ ShiftMask,	                XK_Print,  spawn,          {.v = printscf } },
+	{ MODKEY|ShiftMask,	        XK_Print,  spawn,          {.v = printscf } },
 	{ MODKEY|ShiftMask,             XK_z,      spawn,          {.v = voldown } },
 	{ MODKEY|ShiftMask,             XK_x,      spawn,          {.v = volup } },
 	TAGKEYS(                        XK_1,                      0)
